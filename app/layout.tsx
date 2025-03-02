177f89ef485cc826227d4661a8eb2dcfa6cca51c
Manish Yadav <01devmanish@gmail.com>
2025-03-02 10:37:41 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
@@ -28,6 +29,8 @@ export default function RootLayout({
       <head>
         {/* Set the favicon/logo */}
         <link rel="icon" href="/cv.png" />
+        <Analytics/>
+        <SpeedInsights/>
       </head>
       <body className={` ${inter.variable} ${spaceGrotesk.variable}`}>
         {children}
