faa772d58f9b1607ae5be28aa64efd86e430aabb
Manish Yadav <01devmanish@gmail.com>
2025-03-02 10:32:54 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
